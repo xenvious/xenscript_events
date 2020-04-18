ee552f91b5e38dc148fdfd1398e0c6a4a39c7f45
xenvious <vindictful@gmail.com>
2020-04-18 05:40:06 -0400

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,4 @@
+RegisterServerEvent('xenscript_event:enteringVehicle')
+RegisterServerEvent('xenscript_event:enteringAborted')
+RegisterServerEvent('xenscript_event:enteredVehicle')
+RegisterServerEvent('xenscript_event:leftVehicle')
